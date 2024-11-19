441fe8b29b9258813dedb7d58a99f914a3845227
JrRoca23 <johnnyroca9@gmail.com>
2024-11-19 16:49:09 +0100

    logo changes

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -4,10 +4,9 @@ import './Navbar.css';
 
 function Navbar() {
   const [isOpen, setIsOpen] = useState(false);
-  const location = useLocation(); // Hook para obtener la ruta actual
-  const navigate = useNavigate(); // Hook para navegar programáticamente
+  const location = useLocation(); 
+  const navigate = useNavigate(); 
 
-  // Función para alternar el menú hamburguesa
   const toggleMenu = () => {
     setIsOpen(!isOpen);
   };
@@ -30,53 +29,63 @@ function Navbar() {
 
   // Función para recargar la página cuando el usuario haga clic en "Home"
   const handleHomeClick = () => {
-    navigate('/'); // Navegar a la página de inicio
-    window.location.reload(); // Forzar recarga de la página
-    setIsOpen(false); // Cerrar el menú al hacer clic
+    navigate('/'); 
+    window.location.reload(); 
+    setIsOpen(false); 
+  };
+
+  // Función para manejar el clic en el logo
+  const handleLogoClick = () => {
+    navigate('/'); 
+    window.location.reload(); 
+    setIsOpen(false); 
   };
 
   return (
     <nav className="navbar">
       <div className="navbar-container">
-        <Link to="/" className="navbar-logo">
+        {}
+        <div className="navbar-logo" onClick={handleLogoClick}>
           <img 
             src="./images/RobocupLogoWeb.png" 
             alt="Logo"
             className="navbar-logo-img" 
           />
-        </Link>
+        </div>
+
         <div className={`navbar-links ${isOpen ? 'open' : ''}`}>
-          {/* Mostrar botón "Home" y recargar la página al hacer clic */}
+          {}
           {location.pathname !== '/' && (
             <Link 
               to="/" 
               className="navbar-button" 
-              onClick={handleHomeClick} // Recargar la página al hacer clic
+              onClick={handleHomeClick} 
             >
               Home
             </Link>
           )}
           
-          {/* Mostrar botón "Articles" solo si no estamos en la página de artículos */}
+          {}
           {location.pathname !== '/articles' && (
             <Link to="/articles" className="navbar-button" onClick={() => setIsOpen(false)}>Articles</Link>
           )}
 
-          {/* Mostrar botón "Projects" solo si no estamos en la página de proyectos */}
+          {}
           {location.pathname !== '/featured-projects' && (
             <Link to="/featured-projects" className="navbar-button" onClick={() => setIsOpen(false)}>Projects</Link>
           )}
 
-          {/* Mostrar botón "Videos" solo si no estamos en la página de videos */}
+          {}
           {location.pathname !== '/videos' && (
             <Link to="/videos" className="navbar-button" onClick={() => setIsOpen(false)}>Videos</Link>
           )}
 
-          {/* Mostrar botón "About Us" solo si no estamos en la página de About Us */}
+          {}
           {location.pathname !== '/about' && (
             <Link to="/about" className="navbar-button" onClick={() => setIsOpen(false)}>About Us</Link>
           )}
         </div>
+
         <div className="navbar-toggle" onClick={toggleMenu}>
           <div></div>
           <div></div>
